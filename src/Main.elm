0c1a500dbfefecc54cd865ec02fd249e12c7a7e5
Felix Watson <felix.dork@gmail.com>
2023-05-10 09:58:42 +1000

    building display for returned order

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, h1, input, p, text)
+import Html exposing (Html, button, div, h1, h3, input, p, text)
 import Html.Attributes exposing (placeholder)
 import Html.Events exposing (onClick, onInput)
 
@@ -9,17 +9,75 @@ import Html.Events exposing (onClick, onInput)
 orderTable : List String -> Html Msg
 orderTable names =
     div []
-        (List.map (\ele -> p [] [ text ele ]) names)
+        (List.map
+            (\ele ->
+                div []
+                    [ p [] [ text ele ]
+                    , button [ onClick (RemoveFromList ele) ] [ text "x" ]
+                    ]
+            )
+            names
+        )
+
+
+removeFromTable : List String -> String -> List String
+removeFromTable list item =
+    List.filter (\x -> not (String.contains item x)) list
+
+
+viewCardsList : List Card -> Html Msg
+viewCardsList cards =
+    div []
+        (List.map
+            (\ele ->
+                div []
+                    [ p [] [ text ele.name ]
+                    , p [] [ text (String.fromFloat ele.price) ]
+                    ]
+            )
+            cards
+        )
+
+
+viewShop : ShopOrder -> Html Msg
+viewShop shop =
+    div []
+        [ h3 [] [ text shop.shopName ]
+        , viewCardsList shop.cards
+        ]
+
+
+viewOptimized : List ShopOrder -> Html Msg
+viewOptimized orderList =
+    div []
+        (List.map viewShop orderList)
 
 
 type Msg
     = SetSearchInput String
     | AddToList String
+    | RemoveFromList String
+
+
+type alias ShopOrder =
+    { shopName : String
+    , cards : List Card
+    }
+
+
+type alias Card =
+    { name : String
+    , price : Float
+    }
 
 
 type alias Model =
     { searchInput : String
     , cardsList : List String
+    , optimizedOrder :
+        { price : Float
+        , arrangement : List ShopOrder
+        }
     }
 
 
@@ -32,13 +90,17 @@ initialModel : Model
 initialModel =
     { searchInput = ""
     , cardsList = []
+    , optimizedOrder =
+        { price = 0
+        , arrangement = []
+        }
     }
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text "Buy Optimizer" ]
+        [ h1 [] [ text "Optimizer" ]
         , input [ placeholder "name", onInput SetSearchInput ] []
         , button [ onClick (AddToList model.searchInput) ] [ text "add" ]
         , orderTable model.cardsList
@@ -52,11 +114,14 @@ update msg model =
             { model | searchInput = newText }
 
         AddToList newCard ->
-            if List.member newCard validCards then
+            if List.member newCard validCards && not (List.member newCard model.cardsList) then
                 { model | cardsList = List.append model.cardsList [ newCard ] }
 
             else
                 model
+
+        RemoveFromList card ->
+            { model | cardsList = removeFromTable model.cardsList card }
 
 
 main : Program () Model Msg
