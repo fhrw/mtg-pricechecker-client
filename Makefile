c27973a624119d6fb82f4373f56be84bfd889ddb
Felix Watson <felix.dork@gmail.com>
2023-05-12 11:50:13 +1000

    changed to browser.element and installed some stuff

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
-make dev:
+.PHONY: dev
+dev: # compiles and opens
 	elm make src/Main.elm
 	open -a Safari index.html
+
+.PHONY: build
+build: #just compiles
+	elm make src/Main.elm
--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,91 @@ import Browser
 import Html exposing (Html, button, div, h1, h3, input, p, text)
 import Html.Attributes exposing (placeholder)
 import Html.Events exposing (onClick, onInput)
+import List.Extra exposing (remove)
+import Platform.Cmd as Cmd
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = \flags -> ( initialModel, Cmd.none )
+        , subscriptions = subscriptions
+        , view = view
+        , update = update
+        }
+
+
+
+-- MODEL
+
+
+validCards : List String
+validCards =
+    [ "island", "forest", "plains" ]
+
+
+type alias Model =
+    { searchInput : String
+    , cardsList : List String
+    , optimizedOrder :
+        { price : Float
+        , arrangement : List ShopOrder
+        }
+    }
+
+
+type alias ShopOrder =
+    { shopName : String
+    , cards : List Card
+    }
+
+
+type alias Card =
+    { name : String
+    , price : Float
+    }
+
+
+initialModel : Model
+initialModel =
+    { searchInput = ""
+    , cardsList = []
+    , optimizedOrder =
+        { price = 0
+        , arrangement = []
+        }
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = SetSearchInput String
+    | AddToList String
+    | RemoveFromList String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        SetSearchInput newText ->
+            ( { model | searchInput = newText }, Cmd.none )
+
+        AddToList newCard ->
+            if List.member newCard validCards && not (List.member newCard model.cardsList) then
+                ( { model | cardsList = List.append model.cardsList [ newCard ] }, Cmd.none )
+
+            else
+                ( model, Cmd.none )
+
+        RemoveFromList card ->
+            ( { model | cardsList = removeFromTable model.cardsList card }, Cmd.none )
 
 
 orderTable : List String -> Html Msg
@@ -20,9 +105,33 @@ orderTable names =
         )
 
 
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text "Optimizer" ]
+        , input [ placeholder "name", onInput SetSearchInput ] []
+        , button [ onClick (AddToList model.searchInput) ] [ text "add" ]
+        , orderTable model.cardsList
+        , viewOptimized model.optimizedOrder.arrangement
+        ]
+
+
 removeFromTable : List String -> String -> List String
 removeFromTable list item =
-    List.filter (\x -> not (String.contains item x)) list
+    remove item list
 
 
 viewCardsList : List Card -> Html Msg
@@ -51,84 +160,3 @@ viewOptimized : List ShopOrder -> Html Msg
 viewOptimized orderList =
     div []
         (List.map viewShop orderList)
-
-
-type Msg
-    = SetSearchInput String
-    | AddToList String
-    | RemoveFromList String
-
-
-type alias ShopOrder =
-    { shopName : String
-    , cards : List Card
-    }
-
-
-type alias Card =
-    { name : String
-    , price : Float
-    }
-
-
-type alias Model =
-    { searchInput : String
-    , cardsList : List String
-    , optimizedOrder :
-        { price : Float
-        , arrangement : List ShopOrder
-        }
-    }
-
-
-validCards : List String
-validCards =
-    [ "island", "forest", "plains" ]
-
-
-initialModel : Model
-initialModel =
-    { searchInput = ""
-    , cardsList = []
-    , optimizedOrder =
-        { price = 0
-        , arrangement = []
-        }
-    }
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ h1 [] [ text "Optimizer" ]
-        , input [ placeholder "name", onInput SetSearchInput ] []
-        , button [ onClick (AddToList model.searchInput) ] [ text "add" ]
-        , orderTable model.cardsList
-        , viewOptimized model.optimizedOrder.arrangement
-        ]
-
-
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        SetSearchInput newText ->
-            { model | searchInput = newText }
-
-        AddToList newCard ->
-            if List.member newCard validCards && not (List.member newCard model.cardsList) then
-                { model | cardsList = List.append model.cardsList [ newCard ] }
-
-            else
-                model
-
-        RemoveFromList card ->
-            { model | cardsList = removeFromTable model.cardsList card }
-
-
-main : Program () Model Msg
-main =
-    Browser.sandbox
-        { init = initialModel
-        , view = view
-        , update = update
-        }
